704a6fd62f9b7c037de8b20fd4284d86478b15e6
Ваня <brazik1031@icloud.com>
2025-01-11 15:35:12 +0300

    start homework

---
+++ src/main/java/com/prosky/springCart/SpringCartApplication.java
@@ -0,0 +1,13 @@
+package com.prosky.springCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prosky/springCart/SpringCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosky.springCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
